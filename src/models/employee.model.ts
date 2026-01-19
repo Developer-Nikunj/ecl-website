6b0bb88c9a7735ed64c4a765ec267af190a23321
Shiv <newrajkaushal232@gmail.com>
2026-01-19 11:10:24 +0530

    First project commit

---
+++ src/models/employee.model.ts
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "@/database/db";
+import { id } from "zod/locales";
+
+export const employeeModel = sequelize.define(
+    "employee",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            unique: true,
+             allowNull: false,
+        },
+        employeeNmame: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        employeeEmail: {
+            type: DataTypes.STRING,
+        },
+        Designation: {
+            type: DataTypes.STRING,
+        },
+        Status : {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+        Experience : {
+            type: DataTypes.INTEGER,
+        },
+        Rating : {
+            type: DataTypes.FLOAT,
+        },
+        employeeImg : {
+            type: DataTypes.STRING,
+            defaultValue : 'https://icon-library.com/images/employee-icon-png/employee-icon-png-29.jpg',
+        },
+        
+
+    },
+    { tableName: "employee", freezeTableName: true, }
+);
+
+await employeeModel.sync();
